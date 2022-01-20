5a1d0179a87ad773a7ed1f97d762762246f6093d
徐立 <xuli@uniwyse.com>
2022-01-20 18:32:00 +0800

    create table sql

--- model/group.go
+++ model/group.go
@@ -13,6 +13,19 @@ type GroupQueryParam struct {
 	Limit   int    `form:"limit"`
 }
 
+/*
+UserGroup
+CREATE TABLE `user_group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id_group_id` (`user_id`,`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+*/
 type UserGroup struct {
 	Model
 	UserId  int    `json:"uid,omitempty" uri:"uid" `
